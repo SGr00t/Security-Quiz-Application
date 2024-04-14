c135e2f77b2e47097592fe1ce1baeae7f6573031
Srijib Ghosh <143192352+Srijibrute@users.noreply.github.com>
2024-04-14 13:18:44 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does the acronym AES stand for?",
+    answer: "Advanced Encryption Standard",
+    options: [
+      "Automated Encryption Strength",
+      "Advanced Encryption Standard",
+      "Algorithm Encrypted System",
+      "Advanced Encryption Solution "
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the primary purpose of a VPN (Virtual Private Network) in cyber security?",
+    answer: "Encrypt and secure communication over the internet",
+    options: [
+      "Speed up internet connection",
+      "Encrypt and secure communication over the internet",
+      "Block malicious websites",
+      "Boost Wi-Fi signal strength"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What type of software infects a machine, locks the files, then asks for money?",
+    answer: "Ransomware",
+    options: [
+      "Worm",
+      "Trojan",
+      "Ransomware",
+      "Browser Hijacker"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the purpose of a CAPTCHA in online security?",
+    answer: "Preventing automated bots",
+    options: [
+      "Detecting malware",
+      "Authenticating users",
+      "Preventing automated bots",
+      "Encrypting data"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of these groups exploit vulnerabilities?",
+    answer: "All Of The Above",
+    options: [
+      "Governments",
+      "Criminals",
+      "Hacktivists",
+      "All Of The Above"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which of the following is not a Cybercrime?",
+    answer: "AES",
+    options: [
+      "Denial of Service",
+      "Man in the Middle",
+      "Malware",
+      "AES"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which of the following is a type of Cyber attack?",
+    answer: "All of the above",
+    options: [
+      "Phishing",
+      "SQL Injections",
+      "Password Attack",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which software is mainly used to help users detect viruses and avoid them?",
+    answer: "Antivirus",
+    options: [
+      "Adware",
+      "Malware",
+      "Antivirus",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Identify the port used to connect to Active Directory in Windows 2000.",
+    answer: "389",
+    options: [
+      "139",
+      "445",
+      "80",
+      "389"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Choose among the following techniques, which are used to hide information inside a picture.",
+    answer: "Steganography",
+    options: [
+      "Image rendering",
+      "Steganography",
+      "Rootkits",
+      "Bitmapping"
+    ]
+  },
+];
